0a835117dd0db926d21b48538d36fef293e0da44
Brian Kundinger <briankundinger@gmail.com>
2023-04-19 13:20:08 -0400

    SimulatePaths

--- bk_workspace/Utilities/SimulatePath.py
+++ bk_workspace/Utilities/SimulatePath.py
@@ -1,27 +1,33 @@
 import numpy as np
 
-def SimulatePath(start_location, start_time, end_time, bernoulli_mean, poisson_mean, 
+""" bernoulli_mean = ra_bern_mean
+poisson_mean = ra_pois_mean
+start_time = 0
+end_time = TActual
+location = 26 """
+
+def SimulatePath(location, start_time, end_time, bernoulli_mean, poisson_mean, 
                  unique_edges):
-    N = unique_edges[0]
-    path = np.zeros(end_time - start_time)
+    N = unique_edges.shape[0]
+    path = np.zeros(end_time - start_time + 1)
     i = 0
-    possible_steps = [n for n in np.arange(0, N) if unique_edges[n, 0] == start_location]
+    path[i] = location
+    possible_steps = [n for n in np.arange(0, N) if unique_edges[n, 0] == location]
     for t in np.arange(start_time, end_time):
+        i = i + 1
         Z = [np.random.binomial(1, bernoulli_mean[t, n]) for n in possible_steps]
         # We need a better way to handle the case when all Zs are zero.
         #############################################################
         if all(z == 0 for z in Z):
             path[i] = location
-            i = i + 1
             continue
-        """ Z = np.ones(len(possible_steps)) """
-        ##############################################################
-        z_possible = [n for n, z in zip(realized_edges, Z) if z == 1]
-        rates = poisson_mean[:, n][z_possible]
+        z_possible = [n for n, z in zip(possible_steps, Z) if z == 1]
+        rates = poisson_mean[t, :][z_possible]
         probs = rates / sum(rates)
         location_index = np.random.choice(z_possible, 1, True, probs)
         location = unique_edges[location_index, 1]
         path[i] = location
-        i = i + 1
         possible_steps = [n for n in np.arange(0, N) if unique_edges[n, 0] == location]
     return(path)
+
+""" Z = np.ones(len(possible_steps)) """
